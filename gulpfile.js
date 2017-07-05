ca31eda419b681362f5561f7196c165a4a580e4b
Ivan <Skayline07@mail.ru>
2017-07-05 16:18:20 +0300

    #100
    Added template files

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var pug = require('gulp-pug');
+var minify = require('gulp-minify');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function(){
+  return gulp.src('src/css/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('www/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('css', function(){
+  return gulp.src('src/css/*.css')
+    .pipe(gulp.dest('www/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('pug', function(){
+  return gulp.src('src/pug/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('www'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('media', function() {
+  return gulp.src('src/res/**/*')
+    .pipe(gulp.dest('www/res'))
+});
+
+//gulp.task('js', function() {
+//  return gulp.src('src/js/**/*')
+//  .pipe(gulp.dest('www/js'))
+//});
+
+gulp.task('compress', function() {
+  return gulp.src('src/js/**/*.js')
+    .pipe(minify({
+      ignoreFiles: ['*.combo.js', '*.min.js', 'min.js'],
+      ext:{
+        src:'.js',
+        min:'.min.js'
+      },
+      exclude: ['tasks']
+    }))
+    .pipe(gulp.dest('www/js'))
+});
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'www'
+    },
+    ghostMode: {
+      clicks: true,
+      forms: true,
+      scroll: false
+    },
+    open: false,
+    reloadOnRestart: false,
+    notify: false,
+    reloadDelay: 300
+  })
+});
+
+//gulp.task('default', ['compress']);
+gulp.task('watch', ['sass', 'pug', 'media', 'compress', 'css', 'browserSync'], function (){
+  gulp.watch('src/css/*.scss', ['sass']);
+  gulp.watch('src/pug/**/*.pug', ['pug']);
+  //gulp.watch('src/js/**/*.js', ['js']);
+  gulp.watch('src/res/**/*', ['media']);
+  gulp.watch('src/js/**/*.js', ['compress']);
+  gulp.watch('src/css/*.css', ['css']);
+});
